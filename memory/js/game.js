00fc816fadc62b061150d8a07b5af9d7c913a6e4
Amanda Gouveia <amandasmithgouveia@gmail.com>
2022-02-05 17:29:32 -0500

    Set up memory game elements

---
+++ memory/js/game.js
@@ -0,0 +1,68 @@
+const ICONS_ACCESSIBILITY = [
+    "accessible-icon",
+    "american-sign-language-interpreting",
+    "assistive-listening-systems",
+    "audio-description",
+    "blind",
+    "braille",
+    "closed-captioning",
+    "deaf",
+    "low-vision",
+    "phone-volume",
+    "question-circle",
+    "sign-language",
+    "tty",
+    "universal-access",
+    "wheelchair"
+]
+
+const ICONS_ANIMALS = [
+    "cat",
+    "crow",
+    "dog",
+    "dove",
+    "dragon",
+    "feather",
+    "fish",
+    "frog",
+    "hippo",
+    "horse",
+    "kiwi-bird",
+    "otter",
+    "paw",
+    "spider"
+]
+
+const ICONS_RELIGIONS = [
+    "ankh",
+    "atom",
+    "bahai",
+    "bible",
+    "church",
+    "cross",
+    "dharmachakra",
+    "dove",
+    "gopuram",
+    "hamsa",
+    "hanukiah",
+    "journal-whills",
+    "kaaba",
+    "khanda",
+    "menorah",
+    "mosque",
+    "om",
+    "peace",
+    "place-of-worship",
+    "pray",
+    "praying-hands",
+    "quran",
+    "star-and-crescent",
+    "star-of-david",
+    "synagogue",
+    "torah",
+    "torii-gate",
+    "vihara",
+    "yin-yang"
+]
+
+let numPairs = 8
